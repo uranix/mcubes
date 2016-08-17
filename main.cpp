afa49603727a17a03236354fd7ff2307b2215b8e
Ivan Tsybulin <tsybulinhome@gmail.com>
2016-08-17 11:14:44 +0300

    Refactored to move logic into SurfaceMesh

--- main.cpp
+++ main.cpp
@@ -1,20 +1,14 @@
 #include <iostream>
 
-struct point;
-std::ostream &operator<<(std::ostream &o, const point &p);
-
 #include "vtk.h"
 #include "mesh.h"
-
-std::ostream &operator<<(std::ostream &o, const point &p) {
-    return o << "(" << p.x << ", " << p.y << ", " << p.z << ")";
-}
 
 int main() {
     std::vector<point> pts;
 //    std::vector<triangle> tri;
     std::vector<quad> quads;
     VoxelMesh vm(dim3(40, 40, 40), point(0, 0, 0), point(1, 1, 1));
+    EdgeMesh em(vm.n);
 
     std::vector<size_t> ptsoffs(vm.val.size());
 
@@ -24,13 +18,13 @@ int main() {
             const point &dp2 = p - point(.4, .6, .6);
             vm[vox] = 1. / dp1.dot(dp1) + 2. / dp2.dot(dp2);
         });
-    vm.for_each_cube([&pts, &ptsoffs] (VoxelMesh &vm, const dim3 &cube) {
+    vm.for_each_cube([&pts, &ptsoffs, &em] (VoxelMesh &vm, const dim3 &cube) {
             double level = 30;
             ptsoffs[cube.linear_index(vm.n)] = pts.size();
-            vm.gen_vertices(cube, level, pts);
+            vm.gen_vertices(cube, level, pts, em);
         });
 //    vm.for_each_edge([&tri, &ptsoffs] (
-    vm.for_each_edge([&quads, &ptsoffs] (
+    vm.for_each_edge(em, [&quads, &ptsoffs] (
                 const VoxelMesh &vm,
                 const std::array<char, 4> &edgedata,
                 const dim3 cubes[4])
--- mesh.h
+++ mesh.h
@@ -6,29 +6,17 @@
 #include "geom.h"
 #include "lut.h"
 
-struct VoxelMesh {
+struct VoxelMesh;
+
+struct EdgeMesh {
     const dim3 n;
-    std::vector<double> val;
     std::vector<std::array<char, 4>> _xedge, _yedge, _zedge;
-    const point ll, ur, h;
-    VoxelMesh(const dim3 &n, const point &ll, const point &ur)
+    EdgeMesh(const dim3 &n)
         : n(n),
-        val(n.i * n.j * n.k),
         _xedge((n.i - 1) * n.j * n.k),
         _yedge(n.i * (n.j - 1) * n.k),
-        _zedge(n.i * n.j * (n.k - 1)),
-        ll(ll), ur(ur),
-        h((ur.x - ll.x) / n.i, (ur.y - ll.y) / n.j, (ur.z - ll.z) / n.k)
+        _zedge(n.i * n.j * (n.k - 1))
     {
-    }
-    double &operator[](const dim3 &idx) {
-        return val[idx.linear_index(n)];
-    }
-    const double &operator[](const dim3 &idx) const {
-        return val[idx.linear_index(n)];
-    }
-    const point center(const dim3 &idx) const {
-        return point(idx, ll, h);
     }
     std::array<char,4> &xedge(const dim3 &idx) {
         return _xedge[idx.linear_index(n - dim3::X())];
@@ -37,6 +25,15 @@ struct VoxelMesh {
         return _yedge[idx.linear_index(n - dim3::Y())];
     }
     std::array<char,4> &zedge(const dim3 &idx) {
+        return _zedge[idx.linear_index(n - dim3::Z())];
+    }
+    const std::array<char,4> &xedge(const dim3 &idx) const {
+        return _xedge[idx.linear_index(n - dim3::X())];
+    }
+    const std::array<char,4> &yedge(const dim3 &idx) const {
+        return _yedge[idx.linear_index(n - dim3::Y())];
+    }
+    const std::array<char,4> &zedge(const dim3 &idx) const {
         return _zedge[idx.linear_index(n - dim3::Z())];
     }
     void update_edge(const dim3 &cube, const edge &e, int edgeid, bool cb, bool ce, int patchid) {
@@ -56,12 +53,34 @@ struct VoxelMesh {
             edgeinfo[ab] = sig;
         }
     }
+};
+
+struct VoxelMesh {
+    const dim3 n;
+    std::vector<double> val;
+    const point ll, ur, h;
+    VoxelMesh(const dim3 &n, const point &ll, const point &ur)
+        : n(n),
+        val(n.i * n.j * n.k),
+        ll(ll), ur(ur),
+        h((ur.x - ll.x) / n.i, (ur.y - ll.y) / n.j, (ur.z - ll.z) / n.k)
+    {
+    }
+    double &operator[](const dim3 &idx) {
+        return val[idx.linear_index(n)];
+    }
+    const double &operator[](const dim3 &idx) const {
+        return val[idx.linear_index(n)];
+    }
+    const point center(const dim3 &idx) const {
+        return point(idx, ll, h);
+    }
     const point edge_point(const dim3 &cube, const edge &e, double w) const {
         point p1(cube + e.beg, ll, h);
         point p2(cube + e.end, ll, h);
         return p1 * (1 - w) + p2 * w;
     }
-    int gen_vertices(const dim3 &cube, double level, std::vector<point> &pts) {
+    int gen_vertices(const dim3 &cube, double level, std::vector<point> &pts, EdgeMesh &em) const {
         double v[8];
         point newpts[4];
         int cnt[4] = {0, 0, 0, 0};
@@ -82,7 +101,7 @@ struct VoxelMesh {
             double v2 = v[e.end.vertex_id()];
             bool c1 = v1 > level;
             bool c2 = v2 > level;
-            update_edge(cube, e, i, c1, c2, patchid);
+            em.update_edge(cube, e, i, c1, c2, patchid);
             if (c1 == c2) {
                 assert(patchid == 3);
                 continue;
@@ -118,25 +137,40 @@ struct VoxelMesh {
                     f(*this, vox);
     }
     template<typename EdgeFunctor>
-    void for_each_edge(EdgeFunctor &&f) {
+    void for_each_edge(const EdgeMesh &em, EdgeFunctor &&f) {
         dim3 vox;
         for (vox.k = 1; vox.k < n.k - 1; vox.k++)
             for (vox.j = 1; vox.j < n.j - 1; vox.j++)
                 for (vox.i = 0; vox.i < n.i - 1; vox.i++) {
                     dim3 cubes[4] = {vox, vox - dim3::Y(), vox - dim3::Z(), vox - dim3::Y() - dim3::Z()};
-                    f(*this, xedge(vox), cubes);
+                    f(*this, em.xedge(vox), cubes);
                 }
         for (vox.k = 1; vox.k < n.k - 1; vox.k++)
             for (vox.j = 0; vox.j < n.j - 1; vox.j++)
                 for (vox.i = 1; vox.i < n.i - 1; vox.i++) {
                     dim3 cubes[4] = {vox, vox - dim3::X(), vox - dim3::Z(), vox - dim3::X() - dim3::Z()};
-                    f(*this, yedge(vox), cubes);
+                    f(*this, em.yedge(vox), cubes);
                 }
         for (vox.k = 0; vox.k < n.k - 1; vox.k++)
             for (vox.j = 1; vox.j < n.j - 1; vox.j++)
                 for (vox.i = 1; vox.i < n.i - 1; vox.i++) {
                     dim3 cubes[4] = {vox, vox - dim3::X(), vox - dim3::Y(), vox - dim3::X() - dim3::Y()};
-                    f(*this, zedge(vox), cubes);
+                    f(*this, em.zedge(vox), cubes);
                 }
+    }
+};
+
+template<typename Element>
+struct SurfaceMesh {
+    std::vector<point> pts;
+    std::vector<Element> elems;
+    EdgeMesh em;
+    SurfaceMesh(const VoxelMesh &vm, const double level) : em(vm.n) {
+        std::vector<size_t> ptsoffs(vm.val.size());
+        vm.for_each_cube([this, &ptsoffs, level]
+            (const VoxelMesh &vm, const dim3 &cube) {
+                ptsoffs[cube.linear_index(vm.n)] = pts.size();
+                vm.gen_vertices(cube, level, pts, em);
+            });
     }
 };
